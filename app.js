443ef8fa36c10d228ad6876f3c57f6fe229fd572
Rafael Malave <rafomalave@gmail.com>
2017-09-30 15:18:59 -0700

    add questions and answers to arrays

--- app.js
+++ app.js
@@ -1,76 +1,67 @@
 'use strict';
 
-var user, question, answer, score = 0, answer1, answer2, answer3, answer4, answer5;
+var user, answer6, answer, score = 0;
+
+var questions = [
+  'Do I like videogames?',
+  'Is my favorite hobby photography?',
+  'Do I have any pets?',
+  'Do I like to go hiking?',
+  'Do I paint?'
+];
+
+var answers = [
+  'yes',
+  'yes',
+  'no',
+  'yes',
+  'no',
+];
+
+var responses = [
+  'Yes! I love to play games! When I have time that is.',
+  'Right! I love hiking/backpacking to beautifull places and taking photos!',
+  'Nope. But I would love to have a dog in the future!',
+  'Yes! you probably remember I mentioned I like to take photos when I hike ;)',
+  'Nope! Sadly. I would love to learn to paint.'
+];
 
 user = prompt('Hi, what is your name?').toLowerCase();
 console.log('user = ' + user);
 
 alert(user + ', welcome to my guessing game. Let\'s see if you can guess a few things about me');
 
-answer1 = prompt('Do I like videogames? Type yes or no').toLowerCase();
-if(answer1 === 'yes' || answer1 === 'y') {
-  score++;
-  alert('Score: ' + score + '\nOh yeah. I love me some videogames.');
-  console.log('answer1 = ' + answer1);
-  console.log('score = ' + score);
-} else {
-  alert('Score: ' + score + '\nWrong answer! I love videogames!');
-  console.log('answer1 = ' + answer1);
-  console.log('score = ' + score);
+/*
+for(var i = 0; i < questions.length; i++) {
+  answer = prompt(questions[i] + '( yes or no).').toLowerCase();
+  console.log(answer);
+  if(answer === answers[i]) {
+    score++;
+    alert(responses[i] + ' Score: ' + score);
+  } else {
+    alert('Wrong answer!');
+  }
+}
+*/
+
+answer6 = parseInt(prompt('What is my favorite number. HINT: between 2-6.'));
+while(answer6 !== 3) {
+  if(answer6 < 3) {
+    alert('Too low! try again?');
+    answer6 = parseInt(prompt('What is my favorite number. HINT: between 2-6.'));
+  } else if(answer6 > 3) {
+    alert('Too high! try again?');
+    answer6 = parseInt(prompt('What is my favorite number. HINT: between 2-6.'));
+  }
 }
 
-answer2 = prompt('Is Rafael\s favorite hobby photography? Type yes or no').toLowerCase();
-if(answer2 === 'yes' || answer2 === 'y') {
-  score++;
-  alert('Score: ' + score + '\nPhotography is one of my favorite hobbies');
-  console.log('answer1 = ' + answer2);
-  console.log('score = ' + score);
+alert('Yes! 3 is my favorite number');
+/*
+if(score === answers.length) {
+  alert('Perfect Score! Score: ' + score + '/' + answers.length);
+} else if((score / answers.length) * 100 > 0.75) {
+  alert('Nice! you got most of them! Score: ' + score + '/' + answers.length);
 } else {
-  alert('Score: ' + score + '\nWrong! I love photography!');
-  console.log('answer2 = ' + answer2);
-  console.log('score = ' + score);
+  alert('Well, that was a good try. Score: ' + score + '/' + answers.length);
 }
-
-answer3 = prompt('Do you think I like to exercise? Type yes or no').toLowerCase();
-if(answer3 === 'yes' || answer3 === 'y') {
-  score++;
-  alert('Score: ' + score + '\nNice job! I love to exercise.');
-  console.log('answer3 = ' + answer3);
-  console.log('score = ' + score);
-} else {
-  alert('Score: ' + score + '\nWrong! I love to exercise!');
-  console.log('answer3 = ' + answer3);
-  console.log('score = ' + score);
-}
-
-answer4 = prompt('Do you think I like beer? Type yes or no').toLowerCase();
-if(answer4 === 'yes' || 'y') {
-  score++;
-  alert('Score: ' + score + '\nYes! I love to open a cold one after a long day at work.');
-  console.log('answer4 = ' + answer4);
-  console.log('score = ' + score);
-} else {
-  alert('Score: ' + score + '\nWrong! I like a beer after a long day at work.');
-  console.log('answer4 = ' + answer4);
-  console.log('score = ' + score);
-}
-
-answer5 = prompt('Is my favorite color blue? Type yes or no').toLowerCase();
-if(answer5 === 'yes' || answer5 === 'y') {
-  score++;
-  alert('Score: ' + score + '\nYes! right answer.');
-  console.log('answer4 = ' + answer4);
-  console.log('score = ' + score);
-} else {
-  alert('Score: ' + score + '\nNope! wrong answer.');
-  console.log('answer4 = ' + answer4);
-  console.log('score = ' + score);
-}
-
-if(score === 5) {
-  alert('Nice! Perfect score! Score: ' + score);
-} else if(score >= 3) {
-  alert('Not bad! you guessed most of them! Score: ' + score);
-} else if(score < 3) {
-  alert('Better luck next time! Score: ' + score);
-}
+*/
